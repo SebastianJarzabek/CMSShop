0946b612227a9817e01a583ee0cb0db9fb954114
s.jarzabek <sebastian.jarzabek@outlook.com>
2020-10-13 21:38:24 +0200

    5.

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,13 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
   public class PagesController : Controller
+  {
+    // GET: Admin/Pages
+    public ActionResult Index()
     {
-        // GET: Admin/Pages
-        public ActionResult Index()
-        {
-            return View();
-        }
+
+      // Deklaracja listy stron PAgeVM
+      List<PageVM> pagesList = new List<PageVM>();
+
+
+      //Inicializacja listy PageVM
+      using (Db db = new Db())
+      {
+        pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x=> new PageVM(x)).ToList();
+      }
+
+      //Zwracamy strony do widoku 
+      return View(pagesList);
     }
+  }
 }
