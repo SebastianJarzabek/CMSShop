81331010ae394d95003bc0e5dfbbcee28ccdb159
s.jarzabek <sebastian.jarzabek@outlook.com>
2020-11-08 16:43:53 +0100

    Admin shop cz2. view list categories

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -1,13 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Shop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
   public class ShopController : Controller
+  {
+    // GET: Admin/Shop/Categories
+    public ActionResult Categories()
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
-        {
-            return View();
-        }
+      //Deklaracja listy kategori do wyświetlenia.
+      List<CategoryVM> categoryVMList;
+
+      using (Db db = new Db())
+      {
+        categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+      }
+      return View(categoryVMList);
     }
+  }
 }
