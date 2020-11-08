b39e6f4b123e1e95821e9b28d87c265430e9041b
s.jarzabek <sebastian.jarzabek@outlook.com>
2020-11-08 19:59:23 +0100

    Admin shop 6 - Add category. controller

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using CmsShop.Models.ViewModels.Shop;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
@@ -20,5 +21,41 @@ namespace CmsShop.Areas.Admin.Controllers
       }
       return View(categoryVMList);
     }
+
+    //POST: Admin/Shop/AddNewCategory
+    [HttpPost]
+    public string AddNewCategory(string catName)
+    {
+      string id;
+
+      using (Db db = new Db())
+      {
+        // sprawdzamy czy nazwa kategorii jest unikalna
+        if (db.Categories.Any(x => x.Name == catName))
+        {
+          return "tytulzajety";
+        }
+        CategoryDTO dto = new CategoryDTO();
+        dto.Name = catName;
+        dto.Slug = catName.Replace(" ", "-").ToLower();
+        dto.Sorting = 1000;
+
+        db.Categories.Add(dto);
+        db.SaveChanges();
+
+        // pobranie id 
+        id = dto.Id.ToString();
+      }
+      return id;
+    }
+
+    //POST: Admin/Shop/AddNewCategory
+    [HttpPost]
+    public ActionResult snippet()
+    {
+      return View();
+    }
+
+
   }
 }
